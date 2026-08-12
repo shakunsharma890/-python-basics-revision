56d11d94dd6cb55807aea331114a0a61ab3d3027
Shakun Sharma <shakunsharma890@gmail.com>
2026-08-12 20:07:17 +0530

    Add UserInput.py from python-practice

---
+++ UserInput.py
@@ -0,0 +1,11 @@
+#program to take input from user
+
+print("A Sample program to understand input method in python") 
+
+# How to take input of User name
+name = input("Enter your good name:")
+print("Your Good Name is:", name)
+
+# How to take input of User age
+age = int(input("Enter your age:"))
+print("your age is:", age)
